8771c83d70281e245cc6f9f159acb97191cbae6c
stephane delatorre <s.dlt@hotmail.fr>
2017-08-11 19:54:36 +0200

    test home page on heroku

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -1,6 +1,6 @@
 class Bot < ApplicationRecord
   belongs_to :user
-  has_one :boss_games, dependent: :destroy
+  has_one :boss_game, dependent: :destroy
 
   validates :name, presence: true
   validates :channel, presence: true
