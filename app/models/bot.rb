24153a2bcb9e07d380015cf1ebecec89b474367a
stephane delatorre <s.dlt@hotmail.fr>
2017-08-15 18:56:04 +0200

    allow put and post requests

--- app/models/bot.rb
+++ app/models/bot.rb
@@ -8,6 +8,8 @@ class Bot < ApplicationRecord
 
   before_create :generate_token
 
+  private
+
   def generate_token
     self.token = loop do
       random_token = SecureRandom.urlsafe_base64(nil, false)
