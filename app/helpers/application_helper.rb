2017b29f51073a473469711dc5104e765acb05b4
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-08-16 17:43:12 +0200

    fixed boss avatar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def boss_image(boss)
-    if boss.avatar.nil?
+    if boss.avatar.blank?
       "background-image: linear-gradient(rgba(0,0,0, 0), rgba(0,0,0, 0.2)), url('https://www.wallstreetotc.com/wp-content/uploads/2014/10/facebook-anonymous-app.jpg');"
     else
       "background-image: linear-gradient(rgba(0,0,0, 0), rgba(0,0,0, 0.2)), url('#{boss.avatar}');"
