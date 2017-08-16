4db6eb9f9e4c243034cc2e0de9d9f918ed3a0cd0
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-08-16 16:23:48 +0200

    fixed image helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
   def boss_image(boss)
-    unless boss.avatar.nil?
+    if boss.avatar.nil?
+      "background-image: linear-gradient(rgba(0,0,0, 0), rgba(0,0,0, 0.2)), url('https://www.wallstreetotc.com/wp-content/uploads/2014/10/facebook-anonymous-app.jpg');"
+    else
       "background-image: linear-gradient(rgba(0,0,0, 0), rgba(0,0,0, 0.2)), url('#{boss.avatar}');"
     end
-    "background-image: linear-gradient(rgba(0,0,0, 0), rgba(0,0,0, 0.2)), url('https://www.wallstreetotc.com/wp-content/uploads/2014/10/facebook-anonymous-app.jpg');"
   end
 end
