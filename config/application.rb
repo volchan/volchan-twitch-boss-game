b1abb1ecfaeeaf66ebaf77de443dda61de4edb2c
stephane delatorre <s.dlt@hotmail.fr>
2017-08-26 17:01:09 +0200

    fix redis on heroku

--- config/application.rb
+++ config/application.rb
@@ -18,12 +18,12 @@ Bundler.require(*Rails.groups)
 
 module VolchanTwitchBossGame
   class Application < Rails::Application
-    config.active_job.queue_adapter = :sidekiq
     config.generators do |generate|
       generate.assets false
       generate.helper false
     end
 
+    config.active_job.queue_adapter = :sidekiq
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
