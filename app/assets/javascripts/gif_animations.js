a5587fd5536fdcd93ce438f30d02bbb68d8ef528
stephane delatorre <s.dlt@hotmail.fr>
2017-09-02 19:30:28 +0200

    js fix

--- app/assets/javascripts/gif_animations.js
+++ app/assets/javascripts/gif_animations.js
@@ -32,26 +32,26 @@ function strikeAnim (amount) {
     'http://i.imgur.com/0HU0GFx.gif',
     'http://i.imgur.com/f8aQMPt.gif',
     'http://i.imgur.com/LCYgixP.gif'
-  ]
+  ];
 
   if (amount < 100) {
-    var animLink = power1[Math.floor(Math.random() * power1.length)]
+    var animLink = power1[Math.floor(Math.random() * power1.length)];
   } else if (amount < 500) {
-    var animLink = power100[Math.floor(Math.random() * power100.length)]
+    var animLink = power100[Math.floor(Math.random() * power100.length)];
   } else if (amount < 1000) {
-    var animLink = power500[Math.floor(Math.random() * power500.length)]
+    var animLink = power500[Math.floor(Math.random() * power500.length)];
   } else if (amount < 3000) {
-    var animLink = power1000[Math.floor(Math.random() * power1000.length)]
+    var animLink = power1000[Math.floor(Math.random() * power1000.length)];
   } else {
-    var animLink = power3000[Math.floor(Math.random() * power3000.length)]
+    var animLink = power3000[Math.floor(Math.random() * power3000.length)];
   }
 
-  var img_tag = '<img src="' + animLink + '?a=' + Math.random() + '" id="strike-anim" style="position: absolute; z-index: 100; height: 150%; width: 150%;" alt="">'
-  $('.boss').prepend(img_tag)
+  var img_tag = '<img src="' + animLink + '?a=' + Math.random() + '" id="strike-anim" style="position: absolute; z-index: 100; height: 150%; width: 150%;" alt="">';
+  $('.boss').prepend(img_tag);
 };
 
 function healAnim () {
-  var img_tag = '<img src="http://i.imgur.com/fOvRfRk.gif?a=' + Math.random() + '" id="heal-anim" style="position: absolute; z-index: 100; height: 150%; width: 150%;" alt="">'
-  $('.boss').prepend(img_tag)
-  setTimeout(function () { $('#heal-anim').remove() }, 1000)
+  var img_tag = '<img src="http://i.imgur.com/fOvRfRk.gif?a=' + Math.random() + '" id="heal-anim" style="position: absolute; z-index: 100; height: 150%; width: 150%;" alt="">';
+  $('.boss').prepend(img_tag);
+  setTimeout(function () { $('#heal-anim').remove() }, 1000);
 };
