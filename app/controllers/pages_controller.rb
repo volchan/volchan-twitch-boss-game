94077f0c97780b234a0f4f653c6fb302dc31842b
stephane delatorre <s.dlt@hotmail.fr>
2017-10-07 17:03:24 +0200

    test coinhive

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: %i[home show]
 
   def home; end
+
+  def show
+    render template: "pages/#{params[:page]}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'pages#home'
+  get ':page', to: 'pages#show'
+
 
   resources :bots, only: :show
   resources :bosses, only: :update
