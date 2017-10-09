302d4c4224b9723794110618c69592885775bf7e
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-10-09 16:29:30 +0200

    replaced sigle quotes with double quotes in application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,19 +9,19 @@
 
 $.fn.extend({
   animateCss: function (animationName) {
-    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-    this.addClass('animated ' + animationName).one(animationEnd, function() {
-        $(this).removeClass('animated ' + animationName);
-        $('#strike-anim').remove();
+    var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+    this.addClass("animated" + animationName).one(animationEnd, function() {
+        $(this).removeClass("animated" + animationName);
+        $("#strike-anim").remove();
     });
     return this
   },
   animateFlashes: function (animationName) {
-    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
     var flash = this
-    flash.addClass('animated ' + animationName);
+    flash.addClass("animated" + animationName);
     setTimeout( function () {
-      flash.addClass('animated slideOutRight').one(animationEnd, function () {
+      flash.addClass("animated slideOutRight").one(animationEnd, function () {
         flash.remove();
       });
     }, 5000);
@@ -29,29 +29,29 @@ $.fn.extend({
 });
 
 $(".tab-link").on("click", function(e){
-  var tab_id = $(this).attr('data-tab');
-  $(".tab-link").removeClass('active');
-  $(this).addClass('active');
-  $('.tab-content').removeClass('active');
-  $("#" + tab_id).addClass('active');
-  if (tab_id == 'logs') {
-    $('#new-logs').addClass('hidden').html('0');
+  var tab_id = $(this).attr("data-tab");
+  $(".tab-link").removeClass("active");
+  $(this).addClass("active");
+  $(".tab-content").removeClass("active");
+  $("#" + tab_id).addClass("active");
+  if (tab_id == "logs") {
+    $("#new-logs").addClass("hidden").html("0");
     logHiddenCounter = 0;
   }
 });
 
 function checkFlashes() {
-  if ($('.alert').length > 0) {
-    $('.alert').animateFlashes('slideInRight');
+  if ($(".alert").length > 0) {
+    $(".alert").animateFlashes("slideInRight");
   }
 }
 
 $(document).ready(function(){
-  var clipboard = new Clipboard('.clipboard-btn');
+  var clipboard = new Clipboard(".clipboard-btn");
 
-  $('.clipboard-btn').popover().click(function () {
+  $(".clipboard-btn").popover().click(function () {
     setTimeout(function () {
-        $('.clipboard-btn').popover('hide');
+        $(".clipboard-btn").popover("hide");
     }, 2000);
   });
 
