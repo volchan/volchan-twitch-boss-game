51b8e595b872b0ecfd02643431fc5eb4e1621b91
stephane delatorre <s.dlt@hotmail.fr>
2017-10-11 22:21:28 +0200

    cleaned routes.rb, fixed notifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
 
-  authenticate :user, lambda { |u| u.admin } do
+  authenticate :user, ->(u) { u.admin } do
     mount Sidekiq::Web => '/sidekiq'
   end
 
