bd94f83547a957513b7986af35dcbfd663dd6ee9
stephane delatorre <s.dlt@hotmail.fr>
2017-10-13 16:41:39 +0200

    fix getting logs from two weeks ago

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,5 +1,5 @@
 class Log < ApplicationRecord
   belongs_to :bot
 
-  scope :two_weeks_ago, ->() { where('created_at > ?', 2.weeks.ago) }
+  scope :two_weeks_ago, ->() { where('created_at < ?', 2.weeks.ago) }
 end
