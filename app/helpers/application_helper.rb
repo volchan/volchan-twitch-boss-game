a592bd5690df5514d0371c0144a1d01789f0147c
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2017-10-15 22:23:44 +0200

    reworked "application_helper.rb"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,31 @@
 module ApplicationHelper
   def connected_in_chat?(channel)
-    twitch_api_call = RestClient.get("http://tmi.twitch.tv/group/user/#{channel}/chatters")
-    parsed_twitch_api_call = JSON.parse(twitch_api_call, object_class: OpenStruct)
-    moderator = parsed_twitch_api_call.chatters.moderators.include?(ENV['TWITCH_BOT_NAME'])
-    viewer = parsed_twitch_api_call.chatters.viewers.include?(ENV['TWITCH_BOT_NAME'])
-    moderator || viewer
+    link = "http://tmi.twitch.tv/group/user/#{channel}/chatters"
+    twitch_api_call = api_call(link)
+    parsed_twitch_api_call = parse_call(twitch_api_call)
+    moderator?(parsed_twitch_api_call) || viewer?(parsed_twitch_api_call)
   end
 
   def moderator_in_chat?(channel)
-    twitch_api_call = RestClient.get("http://tmi.twitch.tv/group/user/#{channel}/chatters")
-    parsed_twitch_api_call = JSON.parse(twitch_api_call, object_class: OpenStruct)
-    parsed_twitch_api_call.chatters.moderators.include?(ENV['TWITCH_BOT_NAME'])
+    link = "http://tmi.twitch.tv/group/user/#{channel}/chatters"
+    twitch_api_call = api_call(link)
+    parsed_twitch_api_call = parse_call(twitch_api_call)
+    moderator?(parsed_twitch_api_call)
+  end
+  
+  def api_call(link)
+    RestClient.get(link)
+  end
+  
+  def parse_call(call)
+    JSON.parse(call, object_class: OpenStruct)
+  end
+  
+  def moderator?(parsed_call)
+    parsed_call.chatters.moderators.include?(ENV['TWITCH_BOT_NAME'])
+  end
+  
+  def viewer?(parsed_call)
+    parsed_call.chatters.viewers.include?(ENV['TWITCH_BOT_NAME'])
   end
 end
