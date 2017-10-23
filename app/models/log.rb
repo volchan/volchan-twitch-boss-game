acfe4a4b3c04ea096a8e7826ea3698c159ec3921
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-10-23 18:02:48 +0200

    infinite scrolling done

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,6 +1,8 @@
 class Log < ApplicationRecord
   belongs_to :bot
 
+  paginates_per 30
+
   scope :two_weeks_ago, ->() { where('created_at < ?', 2.weeks.ago) }
   scope :find_with_bot, lambda { |bot|
                           where('bot_id = ?', bot.id)
