d5be7dbdd508c8711fd073c194efb14cf7f0a7e3
stephane DE LA TORRE <s.dlt@hotmail.fr>
2017-10-30 18:05:34 +0100

    added user policies

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,9 +1,10 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def twitch
-      user = User.find_for_twitch_oauth(request.env['omniauth.auth'])
+      user = policy_scope(User.find_for_twitch_oauth(request.env['omniauth.auth']))
 
       if user.persisted?
+        authorize user
         sign_in_and_redirect user, event: :authentication
         return unless is_navigational_format?
         set_flash_message(:notice, :success, kind: 'Twitch')
