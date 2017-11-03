7dc9cfcde0a67246fcddb1457bbbc2ae33fe8795
stephane delatorre <s.dlt@hotmail.fr>
2017-11-03 18:57:59 +0100

    fixed pundit error when logging with twitch

--- app/controllers/dashboard/dashboards_controller.rb
+++ app/controllers/dashboard/dashboards_controller.rb
@@ -1,7 +1,7 @@
 module Dashboard
   class DashboardsController < ApplicationController
     def index
-      return if current_user.bot.nil?
+      return skip_policy_scope if current_user.bot.nil?
       @bot = policy_scope(Bot.find_with_user(current_user))
       @logs = policy_scope(Log.find_with_bot(@bot).page(params[:page]))
     end
