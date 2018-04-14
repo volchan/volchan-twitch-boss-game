a763c5c6882308128e882a25dd096c5dfb47c34e
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2018-04-14 16:33:47 +0200

    Update goal.rb

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -6,7 +6,7 @@ class Goal < ApplicationRecord
   validates :required, presence: true, numericality: { only_integer: true, greater_than: 0 }
 
   enum g_type: { sub_goal: 0, bits_goal: 1 }
-  enum status: { in_progress: 0, achieved: 1 }
+  enum status: { in_progress: 0, achieved: 1, paused: 2 }
 
   after_create :broadcast_to_view
 
